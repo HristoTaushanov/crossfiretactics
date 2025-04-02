60f49fbaf2dfd3d46cf3139b4455aead48fbbd8d
Hristo Taushanov <htaushanov@yahoo.com>
2025-04-02 11:08:05 +0300

    Initial step.

---
+++ src/main/java/com/crossfiretactics/CrossfiretacticsApplication.java
@@ -0,0 +1,13 @@
+package com.crossfiretactics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossfiretacticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossfiretacticsApplication.class, args);
+	}
+
+}
